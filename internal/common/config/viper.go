7ff27d91a67877f2ca0efcb9989f2fc0a370135b
xmhu2001 <2268104085@qq.com>
2024-12-02 15:34:01 +0800

    stripe config

--- internal/common/config/viper.go
+++ internal/common/config/viper.go
@@ -1,11 +1,15 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+)
 
 func NewViperConfig() error {
 	viper.SetConfigName("global")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("../common/config")
+	// viper.EnvKeyReplacer(strings.NewReplacer("_", "-"))
 	viper.AutomaticEnv()
+	_ = viper.BindEnv("stripe-key", "STRIPE_KEY")
 	return viper.ReadInConfig()
 }
