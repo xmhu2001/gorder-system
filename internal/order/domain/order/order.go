ff30b7d90bfa8bffde6b286ff1545659f389596d
xmhu2001 <2268104085@qq.com>
2024-12-03 15:16:20 +0800

    order ports grpc

--- internal/order/domain/order/order.go
+++ internal/order/domain/order/order.go
@@ -1,6 +1,9 @@
 package order
 
-import "github.com/xmhu2001/gorder-system/common/genproto/orderpb"
+import (
+	"errors"
+	"github.com/xmhu2001/gorder-system/common/genproto/orderpb"
+)
 
 type Order struct {
 	ID          string          `json:"id"`
@@ -8,4 +11,37 @@ type Order struct {
 	Status      string          `json:"status"`
 	PaymentLink string          `json:"payment_link"`
 	Items       []*orderpb.Item `json:"items"`
+}
+
+// 业务逻辑写在domain
+func NewOrder(id, customerID, status, paymentlink string, items []*orderpb.Item) (*Order, error) {
+	if id == "" {
+		return nil, errors.New("id is required")
+	}
+	if customerID == "" {
+		return nil, errors.New("customerID is required")
+	}
+	if status == "" {
+		return nil, errors.New("status is required")
+	}
+	if items == nil {
+		return nil, errors.New("items is required")
+	}
+	return &Order{
+		ID:          id,
+		CustomerID:  customerID,
+		Status:      status,
+		PaymentLink: paymentlink,
+		Items:       items,
+	}, nil
+}
+
+func (o *Order) ToProto() *orderpb.Order {
+	return &orderpb.Order{
+		ID:          o.ID,
+		CustomerID:  o.CustomerID,
+		Status:      o.Status,
+		Items:       o.Items,
+		PaymentLink: o.PaymentLink,
+	}
 }
--- internal/order/ports/grpc.go
+++ internal/order/ports/grpc.go
@@ -1,13 +1,21 @@
 package ports
 
 import (
-	context "context"
+	"context"
+	"github.com/sirupsen/logrus"
 	"github.com/xmhu2001/gorder-system/order/app"
+	"github.com/xmhu2001/gorder-system/order/app/command"
+	"github.com/xmhu2001/gorder-system/order/app/query"
+	domain "github.com/xmhu2001/gorder-system/order/domain/order"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/xmhu2001/gorder-system/common/genproto/orderpb"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+// ports 和外界交互的入口
+// payment 的输入（更新order的指令）从这里进来
 type GRPCServer struct {
 	app app.Application
 }
@@ -17,16 +25,42 @@ func NewGRPCServer(app app.Application) *GRPCServer {
 }
 
 func (G GRPCServer) CreateOrder(ctx context.Context, request *orderpb.CreateOrderRequest) (*emptypb.Empty, error) {
-	//TODO implement me
-	panic("implement me")
+	_, err := G.app.Commands.CreateOrder.Handle(ctx, command.CreateOrder{
+		CustomerID: request.CustomerID,
+		Items:      request.Items,
+	})
+	// grpc 中错误需要用 status.Error 库包一层
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	return &emptypb.Empty{}, nil
 }
 
 func (G GRPCServer) GetOrder(ctx context.Context, request *orderpb.GetOrderRequest) (*orderpb.Order, error) {
-	//TODO implement me
-	panic("implement me")
+	o, err := G.app.Queries.GetCustomerOrder.Handle(ctx, query.GetCustomerOrder{
+		CustomerId: request.CustomerID,
+		OrderId:    request.OrderID,
+	})
+	if err != nil {
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+	// handle 返回的是 domain 的 order
+	// 这里返回的是 protobuf 的 order
+	return o.ToProto(), nil
 }
 
-func (G GRPCServer) UpdateOrder(ctx context.Context, order *orderpb.Order) (*emptypb.Empty, error) {
-	//TODO implement me
-	panic("implement me")
+func (G GRPCServer) UpdateOrder(ctx context.Context, request *orderpb.Order) (_ *emptypb.Empty, err error) {
+	logrus.Infof("order_grpc||request_in||request=%+v", request)
+	order, err := domain.NewOrder(request.ID, request.CustomerID, request.Status, request.PaymentLink, request.Items)
+	if err != nil {
+		err = status.Error(codes.Internal, err.Error())
+		return
+	}
+	_, err = G.app.Commands.UpdateOrder.Handle(ctx, command.UpdateOrder{
+		Order: order,
+		UpdateFn: func(ctx context.Context, order *domain.Order) (*domain.Order, error) {
+			return order, nil
+		},
+	})
+	return
 }
